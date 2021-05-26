fb47794a5f948a6ea6281e1cfa2b6b5e00ff70a2
Jasen Hall <jhall@invoca.com>
2021-05-25 20:07:04 -0700

    OPS-3309: Add AWS Metadata to node exporter

---
+++ collector/awsmetadata.go
@@ -0,0 +1,57 @@
+package collector
+
+import (
+	"time"
+	"encoding/json"
+	
+	"github.com/go-kit/kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type sheduled_event struct {
+  Code         string  `json:"code"`
+  State        string  `json:"state"`
+  Description  string  `json:"description"`
+  EventId      string  `json:"eventid"`
+  NotBefore    string  `json:"notbefore"`
+  NotAfter     string  `json:"notafter"`
+}
+
+type awsmetadataCollector struct {
+	logger log.Logger
+}
+
+func init() {
+	registerCollector("awsmetadata", defaultEnabled, NewAwsmetadataCollector)
+}
+
+// NewAwsmetadataCollector returns a new Collector exposing AWS metadata stats
+func NewAwsmetadataCollector(logger log.Logger) (Collector, error) {
+	return &awsmetadataCollector{logger}, nil
+}
+
+func (c *awsmetadataCollector) Update(ch chan<- prometheus.Metric) error {
+	// md_metrics, err := c.getAwsMetadata()
+
+	return nil
+}
+
+func (c *awsmetadataCollector) getAwsMetadata() (map[string]string, error) {
+	return nil, nil
+}
+
+func (c *awsmetadataCollector) getAwsScheduledEvents() (map[string]string, error) {
+	return nil, nil
+}
+
+func parseAwsScheduledEvents(data string) ([]sheduled_event, error) {
+	return nil, nil
+}
+
+func parseAwsScheduledEventMetrics(event sheduled_event) ([3]int, error) {
+/*	if event.State == "active" {
+		
+	}
+*/
+	return [3]int{0, 0, 0}, nil
+}
---
+++ collector/awsmetadata_test.go
@@ -0,0 +1,63 @@
+package collector
+
+import (
+	"testing"
+)
+
+func TestAwsMetadata(t *testing.T) {
+	wantevent := map[string]string {
+		"NotBefore" : "1 May 2021 22:00:00 GMT", 
+		"NotAfter" : "2 May 2021 00:00:00 GMT",
+		"State" : "active",
+	}
+	wantmetrics := map[string]int {
+		"notbefore" : 1619906400,
+		"notafter" : 1619913600,
+		"state" : 1,
+	}
+
+	events, err := parseAwsScheduledEvents(`[ {
+  "NotBefore" : "1 May 2021 22:00:00 GMT",
+  "Code" : "system-reboot",
+  "Description" : "scheduled reboot do-not-complete",
+  "EventId" : "instance-event-0000a0aa0aa0a0aaa",
+  "NotAfter" : "2 May 2021 00:00:00 GMT",
+  "State" : "active"
+} ]`)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, event := range events {
+		if event.State != wantevent["State"] {
+			t.Fatalf("want event State %s, got %s", wantevent["State"], event.State)
+		}
+		if event.NotBefore != wantevent["NotBefore"] {
+			t.Fatalf("want event NotBefore %s, got %s", wantevent["NotBefore"], event.NotBefore)
+		}
+		if event.NotAfter != wantevent["NotAfter"] {
+			t.Fatalf("want event NotAfter %s, got %s", wantevent["NotAfter"], event.NotAfter)
+		}
+
+		eventMetrics, err := parseAwsScheduledEventMetrics(event)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// event state 0/1
+		if eventMetrics[0] != wantmetrics["state"] {
+			t.Fatalf("want metric state %d, got %d", wantmetrics["state"], eventMetrics[0])
+		}
+		// event notbefore epoch time
+		if eventMetrics[1] != wantmetrics["notbefore"] {
+			t.Fatalf("want metric notbefore %d, got %d", wantmetrics["notbefore"], eventMetrics[0])
+		}
+		// event notafter epoch time
+		if eventMetrics[2] != wantmetrics["notafter"] {
+			t.Fatalf("want metric notafter %d, got %d", wantmetrics["notafter"], eventMetrics[0])
+		}
+	}
+
+}
