706a2453c6edead084c61cb01c15ae1d60ff8aea
Jasen Hall <jhall@invoca.com>
2021-07-08 15:24:45 -0700

    OPS-3309: add supporting functionality

--- collector/awsmetadata.go
+++ collector/awsmetadata.go
@@ -4,11 +4,11 @@ import (
 	"time"
 	"encoding/json"
 	
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-type sheduled_event struct {
+type scheduledEvent struct {
   Code         string  `json:"code"`
   State        string  `json:"state"`
   Description  string  `json:"description"`
@@ -22,12 +22,12 @@ type awsmetadataCollector struct {
 }
 
 func init() {
-	registerCollector("awsmetadata", defaultEnabled, NewAwsmetadataCollector)
+	registerCollector("awsmetadata", defaultEnabled, newAwsmetdataCollector)
 }
 
-// NewAwsmetadataCollector returns a new Collector exposing AWS metadata stats
-func NewAwsmetadataCollector(logger log.Logger) (Collector, error) {
-	return &awsmetadataCollector{logger}, nil
+// NewUnameCollector returns new unameCollector.
+func newAwsmetdataCollector(logger log.Logger) (Collector, error) {
+	return &unameCollector{logger}, nil
 }
 
 func (c *awsmetadataCollector) Update(ch chan<- prometheus.Metric) error {
@@ -45,15 +45,35 @@ func (c *awsmetadataCollector) getAwsScheduledEvents() (map[string]string, error
 }
 
 // takes an array of json objects in string format and returns populated structs
-func parseAwsScheduledEvents(data string) ([]sheduled_event, error) {
-	return nil, nil
+func parseAwsScheduledEvents(data string) ([]scheduledEvent, error) {
+	res := []scheduledEvent{}
+	json.Unmarshal([]byte(data), &res)
+
+	return res, nil
 }
 
 // returns metrics in the order {active, notbefore, notafter}
-func parseAwsScheduledEventMetrics(event sheduled_event) ([3]int, error) {
-/*	if event.State == "active" {
-		
+func parseAwsScheduledEventMetrics(event scheduledEvent) ([3]int, error) {
+	var metrics [3]int
+	tformat := "_2 Jan 2006 15:04:05 GMT"
+
+	if event.State == "active" {
+		metrics[0] = 1
+	} else {
+		metrics[0] = 0
 	}
-*/
-	return [3]int{0, 0, 0}, nil
+
+	nb, e := time.Parse(tformat, event.NotBefore)
+	if e != nil {
+		return [3]int{0, 0, 0}, e
+	}
+	na, e := time.Parse(tformat, event.NotAfter)
+	if e != nil {
+		return [3]int{0, 0, 0}, e
+	}
+
+	metrics[1] = int(nb.Unix())
+	metrics[2] = int(na.Unix())
+
+	return metrics, nil
 }
